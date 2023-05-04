b6f1f3525b8e74e75b7e2f7ff9f805568e3ce263
Herrera Daniel <danielherrera2009@hotmail.es>
2023-05-04 17:43:41 -0300

    Guia 10 Encuentro 7

--- Backend 1 - Intro a Java/Guia 10 - Colecciones/Ejercicios/Ejercicio6_Tienda/src/App.java
+++ Backend 1 - Intro a Java/Guia 10 - Colecciones/Ejercicios/Ejercicio6_Tienda/src/App.java
@@ -1,12 +1,48 @@
+// GUIA 10 EJERCICIO 6 - Franco Daniel Herrera
+
+import java.util.Scanner;
 import services.TiendaService;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner read = new Scanner(System.in);
+        Boolean salir = false;
         TiendaService ts = new TiendaService();
-
-        ts.añadirProductos();
-        ts.modificarPrecio();
-        ts.mostrarLista();
         
+        System.out.println("Bienvenide!");
+        do{
+            System.out.println("---------------------------------------------");
+            System.out.println("MENU:");
+            System.out.println("1. Agregar Productos a la lista");
+            System.out.println("2. Modificar precio de Producto");
+            System.out.println("3. Eliminar Producto");
+            System.out.println("4. Mostrar lista de Productos");
+            System.out.println("5. Salir");
+            System.out.println("---------------------------------------------");
+            System.out.print("Ingrese opcion => ");
+            int opcion = read.nextInt();
+            read.nextLine();
+            System.out.println("---------------------------------------------");
+    
+            switch (opcion) {
+                case 1:
+                    ts.añadirProductos();
+                    break;
+                case 2:
+                    ts.modificarPrecio();
+                    break;
+                case 3:
+                    ts.eliminarProducto();
+                    break;
+                case 4:
+                    ts.mostrarLista();
+                    break;
+                case 5:
+                    System.out.println("Cerrando aplicacion..");
+                    salir = true;
+                    break;
+            }
+        }while(salir!=true);   
+        read.close();  
     }
 }
--- Backend 1 - Intro a Java/Guia 10 - Colecciones/Ejercicios/Ejercicio6_Tienda/src/services/TiendaService.java
+++ Backend 1 - Intro a Java/Guia 10 - Colecciones/Ejercicios/Ejercicio6_Tienda/src/services/TiendaService.java
@@ -14,21 +14,22 @@ import java.util.Scanner;
 
 public class TiendaService {
     private Scanner read = new Scanner(System.in);
-    private HashMap<String,Float> listaProductos = new HashMap<>();
+    private HashMap<String,Integer> listaProductos = new HashMap<>();
     
     public void añadirProductos(){
         Boolean salir = false;
         do{
-            System.out.print("Ingrese nombre del producto => ");
-            String nombre = read.nextLine();
-            System.out.print("Ingrese precio del producto => ");
-            Float precio = read.nextFloat();
+            System.out.print("Ingrese nombre del producto => -");
+            String nombre = read.nextLine().toLowerCase();
+            System.out.print("Ingrese precio del producto => $");
+            Integer precio = read.nextInt();
+            read.nextLine();
 
             listaProductos.put(nombre, precio);
             System.out.println("Producto agregado!");
 
             System.out.print("Desea seguir? (s/n) => ");
-            String op = read.next();
+            String op = read.nextLine();
             if(op.equalsIgnoreCase("s") || op.equalsIgnoreCase("si")){
                 System.out.println("Repitiendo..");
             }else if(op.equalsIgnoreCase("n") || op.equalsIgnoreCase("no")){
@@ -41,28 +42,58 @@ public class TiendaService {
         }while(!salir==true);
     }
 
-    public void modificarPrecio(){
-        System.out.print("Ingrese el nombre del producto que desea modificar su precio => ");
-        String nombreB = read.nextLine();
+    public void modificarPrecio(){ 
         if(listaProductos.isEmpty()){
             System.out.println("Aun no hay productos en la lista! Ingreselos primero!");
         }else{
-            for(String nombre: listaProductos.keySet()){
-                if(nombreB.equalsIgnoreCase(nombre)){
-                    System.out.println("Producto encontrado!");
-                    System.out.print("["+nombre+"] Ingrese el nuevo precio => ");
-                    Float precio = read.nextFloat();
-                    listaProductos.put(nombre, precio);
-                }
+            System.out.print("Ingrese el nombre del producto que desea modificar su precio => ");
+            String nombreB = read.nextLine().toLowerCase();
+            if(listaProductos.containsKey(nombreB)){
+                System.out.println("Producto encontrado!");
+                System.out.print("["+mayus1(nombreB)+"] Ingrese el nuevo precio => ");
+                Integer precio = read.nextInt();
+                read.nextLine();
+                listaProductos.put(nombreB, precio);
             }
         }
     }
 
+    public void eliminarProducto(){
+        if(listaProductos.isEmpty()){
+            System.out.println("Aun no hay productos en la lista! Ingreselos primero!");
+        }else{
+            System.out.print("Ingrese el nombre del producto que desea eliminar => ");
+            String nombreB = read.nextLine().toLowerCase();
+            if(listaProductos.containsKey(nombreB)){
+                System.out.println("Producto encontrado!");
+                System.out.println("["+mayus1(nombreB)+"] Se eliminó con éxito! ");
+                listaProductos.remove(nombreB);
+            }
+        }
+        // -Sofia Ceci
+        // public void eliminarProducto(HashMap<String, Float> almacen){
+        //     System.out.println("Ingrese el producto que desea eliminar");
+        //        String producto = read.next();
+        //        if (almacen.containsKey(producto)) {
+        //            almacen.remove(producto);
+        //        }else{
+        //            System.out.println("El producto no se encuentra en stock");
+        //        }
+        //    }
+    }
+
     public void mostrarLista(){
-        for(Map.Entry<String,Float> producto: listaProductos.entrySet()){ // Recorrer un mapa trayendo llave y valor
-            System.out.println("[Producto = "+producto.getKey()+" | Precio = $"+producto.getValue()+"]");
+        if(listaProductos.isEmpty()){
+            System.out.println("Aun no hay productos en la lista! Ingreselos primero!");
+        }else{
+            for(Map.Entry<String,Integer> producto: listaProductos.entrySet()){ // Recorrer un mapa trayendo llave y valor
+                System.out.println("[Producto = "+mayus1(producto.getKey())+" | Precio = $"+producto.getValue()+"]");
+            }
         }
     }
 
-
+    // FUNCION EXTRA
+    public String mayus1(String palabra){
+        return String.valueOf(Character.toUpperCase(palabra.charAt(0))) + palabra.substring(1,palabra.length());
+    }
 }
